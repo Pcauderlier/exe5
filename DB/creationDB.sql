c8d4345c7e9c9b9534955c1ebd3469dbbd63c164
Pcauderlier <pierre.cauderlier@outlook.com>
2024-01-07 11:17:17 +0100

    Ajout des commandes SQL de bases

--- DB/creationDB.sql
+++ DB/creationDB.sql
@@ -8,3 +8,23 @@ CREATE TABLE livres (
 
     PRIMARY KEY (ID_Livre)
 );
+
+-- Commandes SQL de bases : 
+
+-- Permet d'afficher toutes les donnée de la table 'livres':
+-- SELECT * FROM livres
+
+--Afficher toute les donnée du livre dont le titre est 'test'
+-- SELECT *¨FROM livres WHERE Titre = 'test' 
+
+--Afficher toutes les infos sur les titres des collones de la table livres
+--DESCRIBE livres
+
+--Rajouter dans la table livres, les valeurs 'titre auteur et année' danns les collones correspondantes
+--INSERT INTO livres (Titre, Auteur,Anne) VALUES ('titre','auteur','année)
+
+--Modifie la colonne dont l'id est 'id' pour changer le titre, l'auteur et l'année
+--UPDATE livres SET Titre = 'titre' ,Auteur = 'auteur' ,Anne = 'année' WHERE ID_Livre = 'id';
+
+-- Suprime la ligne dont l'id est 'id
+-- DELETE FROM livres WHERE ID_Livre = 'id'
